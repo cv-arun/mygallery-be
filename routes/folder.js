6da94d3793d371f44a67782d022e5a2d4e721407
Arun <cvaruncv1998@gmail.com>
2024-02-04 20:29:27 +0530

    region changed from env

--- routes/folder.js
+++ routes/folder.js
@@ -7,7 +7,7 @@ const multerS3 = require('multer-s3')
 const { S3Client } = require('@aws-sdk/client-s3')
 
 const s3 = new S3Client({
-    region:process.env.AWS_REGION,
+    region:'us-west-2',
     credentials: {
         accessKeyId:process.env.AWS_ACCESS_KEY,
         secretAccessKey:process.env.AWS_SECRET_KEY,
