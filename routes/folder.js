d0559ccfc34f613721fbc3a1b3a15a8669caf416
Arun <cvaruncv1998@gmail.com>
2024-02-05 21:53:33 +0530

    multer limit setted

--- routes/folder.js
+++ routes/folder.js
@@ -7,10 +7,10 @@ const multerS3 = require('multer-s3')
 const { S3Client } = require('@aws-sdk/client-s3')
 
 const s3 = new S3Client({
-    region:'us-west-2',
+    region: 'us-west-2',
     credentials: {
-        accessKeyId:process.env.AWS_ACCESS_KEY,
-        secretAccessKey:process.env.AWS_SECRET_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
     },
 })
 
@@ -22,12 +22,13 @@ const upload = multer({
         s3: s3,
         bucket: process.env.S3_BUCKET,
         metadata: function (req, file, cb) {
-            cb(null, { fieldName: file.fieldname, contentType: file.mimetype ,contentDisposition: 'inline' });
+            cb(null, { fieldName: file.fieldname, contentType: file.mimetype, contentDisposition: 'inline' });
         },
         key: function (req, file, cb) {
             cb(null, `gallery/${Date.now().toString()}.jpeg`)
         }
-    })
+    }),
+    limits: { fileSize: 1024 * 1024 * 15 }
 })
 
 router.post('/upload-image', upload.array('photos', 1), uploadImageAndSave)
