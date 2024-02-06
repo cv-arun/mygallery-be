9e5cf0cd1dc8e8d74d704387ea5e5e984940aeef
Arun <cvaruncv1998@gmail.com>
2024-02-06 21:35:33 +0530

    mongo added again

--- controllers/folder.js
+++ controllers/folder.js
@@ -9,10 +9,10 @@ exports.uploadImageAndSave = async (req, res, next) => {
     try {
         logger(res?.locals?.reqId, FILE_NAME, "upload image start", req.body);
 
-        console.log(req.files)
-        // const file = req.files[0]
-        //  const image = new Folder({imageUrl: file.location})
-        //    await image.save()
+        console.log(req.files,"---------------------------------")
+        const file = req.files[0]
+         const image = new Folder({imageUrl: file.location})
+           await image.save()
 
         
         res.send('file')
